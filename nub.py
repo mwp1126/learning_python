6728206db1af9bd8191c6c2391e181860e30bd11
mwp1126 <47206931+mwp1126@users.noreply.github.com>
2019-02-01 17:49:48 +0800

    if变量
    条件语句

--- nub.py
+++ nub.py
@@ -1,7 +1,25 @@
-s2 = 85
-s1 = 72
-r = (s2-s1)/72*100
-# 小明的成绩从去年的72分提升到了今年的85分，
-# 请计算小明成绩提升的百分点，并用字符串格式化显示出'xx.x%'，只保留小数点后1位：
-#print('%.1f' % r)
-print('%.1f %%' % r)
+# -*- coding: utf-8 -*-
+#声明身高变量
+height = 1.75
+#声明体重变量
+weight = 80.5
+#bmi指数变量
+bmi = weight/height**2
+#打印bmi结果
+print(bmi)
+#bmi的各种变量情况
+if bmi < 18.5: 
+   print('低于18.5：过轻')
+#低于18.5的情况
+if 18.5<bmi<25:
+   print('18.5<bmi<25:正常')
+#bmi正常范围
+if 25<bmi<28:
+   print('25<bmi<28:过重')
+#bmi显示过重
+if 28<bmi<32:
+   print('28<bmi<32:肥胖')
+#bmi显示肥胖
+if bmi > 32:
+   print('bmi>32:严重肥胖')
+#bmi严重肥胖
